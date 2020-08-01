77c1e964aab94d093c65214d55eff5bb893b3268
guardian4k <69091450+guardian4k@users.noreply.github.com>
2020-08-02 08:05:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,7 @@
 import urllib.request
 
 def download():
-    url = 'https://services6.arcgis.com/5jNaHNYe2AnnqRnS/arcgis/rest/services/COVID19_Japan/FeatureServer/0/query?where=%E9%80%9A%E3%81%97%3E0&returnIdsOnly=false&returnCountOnly=false&&f=pgeojson&outFields=*&orderByFields=%E9%80%9A%E3%81%97'
+    url = 'https://services9.arcgis.com/XenOZPW9k4gDSO12/arcgis/rest/services/COVID19_JapanCaseData/FeatureServer/0/query?where=%E9%80%9A%E3%81%97%3E-1&returnIdsOnly=false&returnCountOnly=false&&f=pgeojson&outFields=*&orderByFields=%E9%80%9A%E3%81%97'
     title = 'COVID-19_data.json'
     urllib.request.urlretrieve(url, "{0}".format(title))
     
@@ -11,7 +11,7 @@ import json
 from collections import defaultdict
 import discord
 
-TOKEN = 'corona'
+TOKEN = 'korona'
 CHANNEK_ID = '739097587767836704'
 client = discord.Client()
 
